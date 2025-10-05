16684894ba77ab819f451b4db91f179486eb5438
dhivy <dhivy@DELL>
2025-10-05 07:32:33 +0530

    commit

---
+++ COREJAVA_PROGRAMS/src/DAYEIGHT/interfaces/nestedinterfaces/MyInterface.java
@@ -0,0 +1,9 @@
+package DAYEIGHT.interfaces.nestedinterfaces;
+
+public interface MyInterface {
+	 void calculateArea();
+	    interface MyInnerInterface {
+	       int  id = 20;
+	       void print();     
+	    }
+}
