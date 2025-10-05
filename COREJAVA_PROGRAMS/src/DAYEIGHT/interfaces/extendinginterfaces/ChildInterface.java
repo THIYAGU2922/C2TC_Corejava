0dc5a7cd7b6c4866c078dd41463fb35870667470
dhivy <dhivy@DELL>
2025-10-05 07:09:58 +0530

    commit

---
+++ COREJAVA_PROGRAMS/src/DAYEIGHT/interfaces/extendinginterfaces/ChildInterface.java
@@ -0,0 +1,5 @@
+package DAYEIGHT.interfaces.extendinginterfaces;
+
+public interface ChildInterface extends InterfaceOne {
+	void show();
+}
---
+++ COREJAVA_PROGRAMS/src/DAYEIGHT/interfaces/extendinginterfaces/InterfaceOne.java
@@ -0,0 +1,5 @@
+package DAYEIGHT.interfaces.extendinginterfaces;
+
+interface InterfaceOne{  
+	  void print();  
+	} 
